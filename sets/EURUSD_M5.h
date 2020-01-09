d3841a2e7d72a366a62a959594f25e6cd74e813b
kenorb <kenorb@users.noreply.github.com>
2020-01-09 17:37:27 +0000

    Updates strategy code up to latest standard

---
+++ sets/EURUSD_M5.h
@@ -0,0 +1,24 @@
+//+------------------------------------------------------------------+
+//|                  EA31337 - multi-strategy advanced trading robot |
+//|                       Copyright 2016-2020, 31337 Investments Ltd |
+//|                                       https://github.com/EA31337 |
+//+------------------------------------------------------------------+
+
+// Defines strategy's parameter values for the given pair symbol and timeframe.
+struct Stg_Awesome_EURUSD_M5_Params : Stg_Awesome_Params {
+  Stg_Awesome_EURUSD_M5_Params() {
+    symbol = "EURUSD";
+    tf = PERIOD_M5;
+    Awesome_Shift = 0;
+    Awesome_TrailingStopMethod = 0;
+    Awesome_TrailingProfitMethod = 0;
+    Awesome_SignalOpenLevel = 0;
+    Awesome_SignalBaseMethod = 0;
+    Awesome_SignalOpenMethod1 = 0;
+    Awesome_SignalOpenMethod2 = 0;
+    Awesome_SignalCloseLevel = 0;
+    Awesome_SignalCloseMethod1 = 0;
+    Awesome_SignalCloseMethod2 = 0;
+    Awesome_MaxSpread = 0;
+  }
+};
