d8e06a61c9c196ad44f4336fc09d3043919964a5
kenorb <kenorb@users.noreply.github.com>
2020-12-31 17:57:27 +0000

    Optimize SignalOpenFilterMethod (pf=0.29; dd=5.06; tt=5123)

--- config/EURUSD_M15.h
+++ config/EURUSD_M15.h
@@ -9,7 +9,7 @@ struct Stg_Awesome_Params_M15 : StgParams {
   Stg_Awesome_Params_M15() : StgParams(stg_awesome_defaults) {
     lot_size = 0;
     signal_open_method = -1;
-    signal_open_filter = 1;
+    signal_open_filter = 27;
     signal_open_level = 0;
     signal_open_boost = 0;
     signal_close_method = 2;
