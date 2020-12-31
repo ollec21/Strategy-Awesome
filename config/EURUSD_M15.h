852cb1d0412b947f38f4c41cefe5a170d0218d28
kenorb <kenorb@users.noreply.github.com>
2020-12-31 20:13:55 +0000

    Optimize SignalCloseMethod (pf=0.17; dd=62.20; tt=58769)

--- config/EURUSD_M15.h
+++ config/EURUSD_M15.h
@@ -12,7 +12,7 @@ struct Stg_Awesome_Params_M15 : StgParams {
     signal_open_filter = 1;
     signal_open_level = 0;
     signal_open_boost = 0;
-    signal_close_method = 0;
+    signal_close_method = 2;
     signal_close_level = 0;
     price_stop_method = 0;
     price_stop_level = 2;
